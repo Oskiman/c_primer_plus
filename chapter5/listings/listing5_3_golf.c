7f2b1a10d1060a395ccd4151dc4a604a2730d2e3
Paul <Oskiman@users.noreply.github.com>
2025-02-17 15:11:13 +0000

    Add files via upload

---
+++ chapter5/listings/listing5_3_golf.c
@@ -0,0 +1,13 @@
+// golf.c - golf tournament scorecard
+#include <stdio.h>
+
+int main(void)
+{
+	int jane, tarzan, cheeta;
+
+	cheeta = tarzan = jane = 68;
+	printf("		cheeta	tarzan	   jane\n");
+	printf("First round score %4d %6d %8d\n", cheeta, tarzan, jane);
+
+	return 0;
+}
