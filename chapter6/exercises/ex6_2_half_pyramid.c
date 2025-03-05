070ec83a761e07947d9baa5e36c4094bd51a5e98
Paul <Oskiman@users.noreply.github.com>
2025-03-05 08:10:40 +0000

    Add files via upload

---
+++ chapter6/exercises/ex6_2_half_pyramid.c
@@ -0,0 +1,24 @@
+// Exercise 6.2
+// Use nested loops to produce the following pattern.
+// $
+// $$
+// $$$
+// $$$$
+// $$$$$
+#include <stdio.h>
+
+int main(void)
+{
+	int rows = 5;
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j <= i; j++)
+		{
+			printf("$");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
