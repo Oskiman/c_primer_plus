95be0665987d2de75282356c0a73b3c29d985ee8
Paul <Oskiman@users.noreply.github.com>
2025-03-07 10:05:31 +0000

    Add files via upload

---
+++ chapter6/exercises/ex6_6_reverse_word.c
@@ -0,0 +1,22 @@
+// Exercise 6.6
+// Writa a program that reads a single word into a character array
+// and then prints the word backward.  Hint. Use strlen() to compute 
+// the index of the last character in the array
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char *word_array;
+	printf("Enter a word: ");	// assume valid input
+	scanf("%s", word_array);
+	int length = strlen(word_array);
+	for(int i = length; i >= 0; i--)
+	{
+		printf("%c", word_array[i]);
+	}
+
+	printf("\n");
+
+	return 0;
+}
