807845595748d217af1ddb27b6c10d0b409d1a5d
Paul <Oskiman@users.noreply.github.com>
2025-03-11 10:22:01 +0000

    Add files via upload

---
+++ chapter6/exercises/ex6_13_reverse_input.c
@@ -0,0 +1,20 @@
+// Exercise 6.13
+// Program reads in a line of input & prints it in reverse order
+// Store input in an array of char, assume line no longer than 255
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	//char input[255];	// assume valid input
+	char *input;
+	printf("Enter a line of 255 characters or less: ");
+	scanf("%[^\n]s", input);	// [^\n] means "accept all characters except for \n" so we can allow spaces
+	int length = strlen(input);
+	printf("%d\n", length);
+	for(int i = length; i >= 0; i--)
+		printf("%c", input[i]);
+	printf("\n");
+
+	return 0;
+}
