2743aef5cd55fb40cd2e539c2352e7d8bc41c010
Paul <Oskiman@users.noreply.github.com>
2025-03-31 11:52:48 +0100

    Add files via upload

---
+++ chapter8/listings/main.c
@@ -0,0 +1,20 @@
+// file_eof.c - open a file and display it
+
+#include <stdio.h>
+
+int main()
+{
+	int ch;
+    FILE *fp;
+    char fname[50];     // to hold file name
+    
+    printf("Enter the name of the file: ");
+    scanf("%s", fname);
+    fp = fopen(fname, "r");     // open file for reading
+    // getc(fp) gets a character from the open file
+    while((ch = getc(fp)) != EOF)
+        putchar(ch);
+        
+        fclose(fp);     // close the file
+	return 0;
+}
