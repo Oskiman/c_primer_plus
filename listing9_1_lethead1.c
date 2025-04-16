b3a5e98aaff00eb82f5f3d717ba40c315dea0c08
Paul <Oskiman@users.noreply.github.com>
2025-04-16 16:50:30 +0100

    Add files via upload

---
+++ listing9_1_lethead1.c
@@ -0,0 +1,30 @@
+// lethead1.c
+#include <stdio.h>
+
+#define NAME "MEGATHINK, INC."
+#define ADDRESS "10 Megabuck Plaza"
+#define PLACE "Megapolis, CA 94904"
+#define LIMIT 65
+
+void starbar(void);	// prototype the function
+
+int main(void)
+{
+	starbar();
+	printf("%s\n", NAME);
+	printf("%s\n", ADDRESS);
+	printf("%s\n", PLACE);
+	starbar();
+
+	return 0;
+}
+
+void starbar(void)
+{
+	int count;
+
+	for(count = 1; count <= LIMIT; count++)
+		putchar('*');
+
+	putchar('\n');
+}
