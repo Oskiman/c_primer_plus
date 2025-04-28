9c15a2127f80591ad710044f5de0cc9eb84d1964
Paul <Oskiman@users.noreply.github.com>
2025-04-28 11:39:48 +0100

    Add files via upload

---
+++ chapter9/exercises/ex9_5_largerof.c
@@ -0,0 +1,30 @@
+// Exercise 9.5
+// Write and test a function larger_of() that replaces the contents
+// of two double variables with the maximum of the two values.
+// For example, larger_of(x,y) would reset both x and y to the
+// larger of the two
+#include <stdio.h>
+
+void larger_of(double* x, double* y);
+
+int main(void)
+{
+	double x, y;
+	printf("Enter two numbers: ");		// assume valid input
+	scanf("%lf %lf", &x, &y);
+
+	printf("You entered: %lf %lf\n", x, y);
+
+	larger_of(&x, &y);
+	printf("Now both numbers are: %lf %lf\n", x, y);
+
+	return 0;
+}
+
+void larger_of(double* x, double* y)
+{
+	if(x > y)
+		*y = *x;
+	else
+		*x = *y;
+}
