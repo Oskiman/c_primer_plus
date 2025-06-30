7ad1f41c7962791a497836adddf377f3cd630044
Paul <Oskiman@users.noreply.github.com>
2025-06-30 15:53:23 +0100

    Add files via upload

---
+++ chapter11/listings/listing11_14_str_cat.c
@@ -0,0 +1,19 @@
+// str_cat.c - joins two strings
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 80
+
+int main(void)
+{
+	char flower[SIZE];
+	char addon[] = "s smell like old shoes.";
+
+	puts("What is your favourite flower?");
+	gets(flower);
+	strcat(flower, addon);
+	puts(flower);
+	puts(addon);
+
+	return 0;
+}
